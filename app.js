2ae8fb41ac776b81f6b1994689bc6f38e9961de5
Oleksandr <lysakova@yahoo.com>
2023-10-20 19:15:25 +0200

    deploye test

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import employeeRouter from './routes/employeeRouter.js';
+// import employeeRouter from './routes/employeeRouter.js';
 
 const app = express();
 
 app.use(express.json());
-app.use('/employee', employeeRouter);
+
+app.use('/employees', (req, res, next) => {
+  res.status(200).json({
+    message: 'employees',
+  });
+});
+// app.use('/employees', employeeRouter);
+app.use('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'home',
+  });
+});
 
 export default app;
