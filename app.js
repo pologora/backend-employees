47fa346eba530f624033a35e88e3412efd843f5c
Oleksandr <lysakova@yahoo.com>
2023-10-20 19:55:21 +0200

    add route

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 import express from 'express';
-// import employeeRouter from './routes/employeeRouter.js';
+import employeeRouter from './routes/employeeRouter.js';
 
 const app = express();
 
 app.use(express.json());
 
-// app.use('/employees', employeeRouter);
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'home',
-  });
-});
-
-app.get('/employees', (req, res, next) => {
-  res.status(200).json({
-    message: 'employees',
-  });
-});
+app.use('/employees', employeeRouter);
 
 export default app;
